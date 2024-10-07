4591d7358e3b1fcc1ee0e38f9520f3de9bb465b8
cg-yamada <cg-yamada@users.noreply.github.com>
2024-10-07 11:58:21 +0900

    fix:

--- src/dummy.ts
+++ src/dummy.ts
@@ -1,12 +1,5 @@
 import type { Comic } from "./app/features/comic/types"
 
-export const items = [
-  { id: "1", title: "投稿物Title", createrName: "投稿者名称", image: "images/500x300.png" },
-  { id: "2", title: "投稿物Title", createrName: "投稿者名称", image: "images/500x300.png" },
-  { id: "3", title: "投稿物Title", createrName: "投稿者名称", image: "images/300x600.png" },
-  { id: "4", title: "投稿物Title", createrName: "投稿者名称", image: "images/300x600.png" },
-]
-
 export const dummyComics: Comic[] = [
   {
     id: "1",
